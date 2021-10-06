a3b4563b762065c2c62c37c1b834b39aa896148b
Brian Rodiles <71846845+brianrodiles@users.noreply.github.com>
2021-10-06 09:47:15 -0600

    Update WeatherChecker.java

--- Ch2/WeatherChecker.java
+++ Ch2/WeatherChecker.java
@@ -1,9 +1,29 @@
-import java.util.Scanner;
-public class WeatherChecker{
+  /**   
+   *  Program Name: WeatherChecker.java
+   *   Description: Program asks the user for the temperature
+   *                value and uses if-else statements
+   *                to print out recommendations.
+   * Related class: String 
+   *         Input: Temperature.
+   *          args: N/A
+   *        Output: Recommendation from program.
+   *        Author: Christian Servin, Ph.D.
+   *       Contact: dr.christian.servin@gmail.com
+   *                Copyright 2021, Christian Servin, Ph.D.
+   *                Version 1.0
+   */
+   import java.util.Scanner;
+   public class WeatherChecker{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        
+        // Get temperature form user
         System.out.println("Enter temperature");
         int temp = input.nextInt();
+        input.close();
+        
+        // Print the correct statement
+        // Hot temperatures
         if( temp >= 110)
         {
             System.out.println("Do not go outside!");
@@ -12,6 +32,7 @@ public class WeatherChecker{
         {
             System.out.println("wear shorts and t-shirt");
         }
+        // Cold temperatures
         else if( temp <= 30)
         {
             System.out.println("Do not go outside!");
@@ -20,6 +41,5 @@ public class WeatherChecker{
         {
             System.out.println("wear pants and jacket");
         }
-
     }
 }
