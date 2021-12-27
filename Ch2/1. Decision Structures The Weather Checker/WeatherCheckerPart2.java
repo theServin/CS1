b040811841759162b35df8ff730826b71fbf0ee0
Brian Rodiles <71846845+brianrodiles@users.noreply.github.com>
2021-12-26 21:12:42 -0700

    Add files via upload

---
+++ Ch2/1. Decision Structures The Weather Checker/WeatherCheckerPart2.java
@@ -0,0 +1,45 @@
+  /**   
+   *  Program Name: WeatherCheckerPart2.java
+   *   Description: Program asks the user for the temperature
+   *                value and uses if-else statements
+   *                to print out recommendations.
+   * Related class: String 
+   *         Input: Temperature.
+   *          args: N/A
+   *        Output: Recommendation from program.
+   *        Author: Christian Servin, Ph.D.
+   *       Contact: dr.christian.servin@gmail.com
+   *                Copyright 2021, Christian Servin, Ph.D.
+   *                Version 1.0
+   */
+   import java.util.Scanner;
+   public class WeatherCheckerPart2{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Get temperature form user
+        System.out.println("Enter temperature");
+        int temp = input.nextInt();
+        input.close();
+
+        // Print the correct statement
+        // Hot temperatures
+        if( temp >= 110)
+        {
+            System.out.println("do not go outside!");
+        }
+        else if( temp >= 80)
+        {
+            System.out.println("wear shorts and t-shirt");
+        }
+        // Cold temperatures
+        else if( temp <= 30)
+        {
+            System.out.println("do not go outside!");
+        }
+        else
+        {
+            System.out.println("wear pants and jacket");
+        }
+    }
+}
---
+++ Ch2/1. Decision Structures The Weather Checker/WeatherCheckerPart3.java
@@ -0,0 +1,41 @@
+  /**   
+   *  Program Name: WeatherCheckerPart3.java
+   *   Description: Program asks the user for the temperature
+   *                value and uses if-else statements
+   *                to print out recommendations.
+   * Related class: String 
+   *         Input: Temperature.
+   *          args: N/A
+   *        Output: Recommendation from program.
+   *        Author: Christian Servin, Ph.D.
+   *       Contact: dr.christian.servin@gmail.com
+   *                Copyright 2021, Christian Servin, Ph.D.
+   *                Version 1.0
+   */
+   import java.util.Scanner;
+   public class WeatherCheckerPart3{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Get temperature form user
+        System.out.println("Enter temperature");
+        int temp = input.nextInt();
+        input.close();
+
+        // Print the correct statement
+        // Hot temperatures
+        if(temp >= 80){
+            if(temp >= 110)
+                System.out.print("do not go outside!");
+            else
+                System.out.print("wear shorts and t-shirt");
+        }
+        // Cold temperatures 
+        else{
+            if(temp <= 30)
+                System.out.print("do not go outside!");
+            else
+                System.out.print("wear pants and jacket");
+        }
+    }
+}
